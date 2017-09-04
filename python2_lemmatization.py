13537e03c2b2b4702c6dfe0202c5f764d0245cc9
Anton Alexeev <anton.alexeev@seedr.com>
2017-09-04 21:19:29 +0300

    LSA, LDA, lemmatization

--- python2_lemmatization.py
+++ python2_lemmatization.py
@@ -1,13 +1,9 @@
 # coding: utf8
 import os
 
-
 from pattern.text.es import parsetree
 import re
-
-# stopwords = set(
-#     "un una unas unos uno sobre todo también tras otro algún alguno alguna algunos algunas ser es soy eres somos
-# sois estoy esta estamos estais estan como en para atras porque por qué estado estaba ante antes siendo ambos pero por poder puede puedo podemos podeis pueden fui fue fuimos fueron hacer hago hace hacemos haceis hacen cada fin incluso primero desde conseguir consigo consigue consigues conseguimos consiguen ir voy va vamos vais van vaya gueno ha tener tengo tiene tenemos teneis tienen el la lo las los su aqui mio tuyo ellos ellas nos nosotros vosotros vosotras si dentro solo solamente saber sabes sabe sabemos sabeis saben ultimo largo bastante haces muchos aquellos aquellas sus entonces tiempo verdad verdadero verdadera cierto ciertos cierta ciertas intentar intento intenta intentas intentamos intentais intentan dos bajo arriba encima usar uso usas usa usamos usais usan emplear empleo empleas emplean ampleamos empleais valor muy era eras eramos eran modo bien cual cuando donde mientras quien con entre sin trabajo trabajar trabajas trabaja trabajamos trabajais trabajan podria podrias podriamos podrian podriais yo aquel".split())
+from gensim import corpora
 
 stopwords = set(map(lambda x: x.strip(), open("stopwords_es.txt", "r+").read().split("\n")))
 
@@ -29,7 +25,7 @@ for text in texts:
                   not word in stopwords and not re.search("\d", word) and not len(word) < 4]
     prepared_texts.append(lemmatized)
 
-from gensim import corpora
+
 dictionary = corpora.Dictionary(prepared_texts)
 
 print("Dictionary built!", dictionary)
@@ -42,7 +38,7 @@ k = 5
 
 import gensim
 
-lda = gensim.models.ldamodel.LdaModel(corpus=corpus,
+ldamodel = gensim.models.ldamodel.LdaModel(corpus=corpus,
                                       id2word=dictionary,
                                       num_topics=k,
                                       update_every=1,
@@ -61,5 +57,12 @@ print("LSI trained")
 for i in range(k):
     print("Topic", i)
     print()
-    print("\n".join([dictionary[term] + "\t" + str(prob) for (term, prob) in lda.get_topic_terms(i, topn=15)]))
+    print("\n".join([dictionary[term].encode("utf-8") + "\t" + str(prob) for (term, prob) in ldamodel.get_topic_terms(i, topn=100000)]))
     print()
+
+with open("dasha-please-dont-send-screenshots-anymore-use-gists-or-whatever-because-i-cant-copy-and-paste.csv", "w") as wf:
+
+    for i in range(k):
+        wf.write(
+            "\n".join([dictionary[term].encode("utf-8") + "," + str(prob) for (term, prob) in ldamodel.get_topic_terms(i, topn=100000)]))
+        wf.write("\n")
